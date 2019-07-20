d73549a43004cbdfd3c3f1a45fd9c10cbb5f547e
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-07-20 23:32:26 +0500

    Boolean logic

--- JS Basics/script.js
+++ JS Basics/script.js
@@ -133,7 +133,7 @@ console.log("Is Mark's BMI higher than John's? " + isHigher);
 /***********************
  * If / else statements
  */
-
+/*
 var firstName = 'John';
 var civilStatus = 'single';
 
@@ -165,4 +165,23 @@ if (bmiMark > bmiJohn) {
     console.log('Mark has more BMI than John');
 } else {
     console.log('John\'s BMI is more than Mark\'s');
+}
+*/
+
+/***********************
+ * Boolean logic
+ * AND (&&), OR (||) & NOT (!)
+ */
+
+var firstName = 'Jahanzeb';
+var age = 20;
+
+if (age < 13) {
+    console.log(firstName + ' is a boy');
+} else if (age >= 13 && age < 20) { //Between 13 and 20
+    console.log(firstName + ' is a teenager');
+} else if (age >= 20 && age < 30) {
+    console.log(firstName + ' is a young man');
+} else {
+    console.log(firstName + ' is a man');
 }

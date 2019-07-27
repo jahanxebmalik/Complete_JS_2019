64b91e28e3dfd72eb572d2f7987b317e082b34df
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-07-27 21:33:40 +0500

    pig game additional challenge

--- DOM Pig Game/app.js
+++ DOM Pig Game/app.js
@@ -44,7 +44,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //Check if player won the game
-        if (scores[activePlayer] >= 20) {
+        if (scores[activePlayer] >= 100) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector(".player-" + activePlayer + '-panel').classList.add('winner');
@@ -60,6 +60,14 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 function nextPlayer() {
     //Next player
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator
+    /* 
+    if(activePlayer === 0) {
+        activePlayer = 1;
+    } else {
+        activePlayer = 0;
+    }
+*/
+
     roundScore = 0;
 
     document.getElementById('current-0').textContent = '0';
@@ -99,8 +107,6 @@ function init() {
 
 }
 
-
-
 //Setting a value
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
@@ -108,3 +114,12 @@ function init() {
 
 //Getting a value
 //var x = document.querySelector('#score-0').textContent;
+
+/*
+YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add a input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good opportunity to use Google to figure this out)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+*/

fb44fb6e57dc5a6b8b83d5b7e65f8c1b408040ae
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-07-28 00:08:08 +0500

    dynamic winning score

--- DOM Pig Game/challenges.js
+++ DOM Pig Game/challenges.js
@@ -61,10 +61,17 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         var input = document.querySelector('.final-score').value;
-        console.log(input);
+        var winningScore;
+        //Undefined, 0, null or "" are COERCED  to false 
+        //Anything else is COERCED to true
+        if (input) {
+            winningScore = input;
+        } else {
+            winningScore = 100;
+        }
 
         //Check if player won the game
-        if (scores[activePlayer] >= 100) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector(".player-" + activePlayer + '-panel').classList.add('winner');

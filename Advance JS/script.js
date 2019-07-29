704175fd0a2ea6c55ae6229804203749216441d0
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-07-29 18:54:44 +0500

    object.create

--- Advance JS/script.js
+++ Advance JS/script.js
@@ -43,6 +43,7 @@ console.log(jane.lastName);
 console.log(mark.lastName);
 */
 //FUNCTION CONSTRUCTOR
+/*
 var LiverpoolPlayers = function (firstName, lastName, age, position, country) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -70,3 +71,31 @@ bobby.retirementAge();
 salah.retirementAge();
 sadio.retirementAge();
 gini.retirementAge();
+*/
+
+// Object.create
+
+var personProto = {
+    calculateAge: function () {
+        console.log(2016 - this.yearOfBirth);
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+//Option 2
+
+var jane = Object.create(personProto, {
+    name: {
+        value: 'Jane'
+    },
+    yearOfBirth: {
+        value: 1968
+    },
+    job: {
+        value: 'designer'
+    }
+})

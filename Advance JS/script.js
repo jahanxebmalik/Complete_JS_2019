9afb8d0cb7b7c26160c80fbae1defd095ff9f72a
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-07-31 02:06:00 +0500

    passing functions as arguments

--- Advance JS/script.js
+++ Advance JS/script.js
@@ -145,40 +145,6 @@ console.log(obj.city);
 
 //Passing functions as arguments
 
-// var years = [1985, 1986, 1988, 1989, 2018];
-
-// function arrayCalc(arr, fn) {
-//     var arrRes = [];
-//     for (var i = 0; i < arr.length; i++) {
-//         arrRes.push(fn(arr[i]));
-//     }
-//     return arrRes;
-// }
-
-// function calculateAge(el) {
-//     return 2019 - el;
-// }
-
-// function isFullAge(el) {
-//     return el >= 18;
-// }
-
-// function maxHeartRate(el) {
-//     if (el >= 18 && el <= 81) {
-//         return Math.round(206.9 - (0.67 * el));
-//     } else {
-//         return -1
-//     }
-// }
-
-// var ages = arrayCalc(years, calculateAge);
-// var fullAges = arrayCalc(ages, isFullAge);
-// var heartRate = arrayCalc(ages, maxHeartRate);
-
-// console.log(ages);
-// console.log(fullAges);
-// console.log(heartRate);
-
 var years = [1985, 1947, 2016, 1965, 1999];
 
 function arrayCalc(arr, fn) {

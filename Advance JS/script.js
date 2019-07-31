63493a2440895919590fe5ce72e287b8cb2f7ac3
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-07-31 18:53:31 +0500

    Functions returning functions

--- Advance JS/script.js
+++ Advance JS/script.js
@@ -144,7 +144,7 @@ console.log(obj.city);
 */
 
 //Passing functions as arguments
-
+/*
 var years = [1985, 1947, 2016, 1965, 1999];
 
 function arrayCalc(arr, fn) {
@@ -177,3 +177,29 @@ var heartRate = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(aboveAge);
 console.log(heartRate);
+*/
+
+// Functions returning functions
+
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('John');
+
+interviewQuestion('designer')('Ghous');

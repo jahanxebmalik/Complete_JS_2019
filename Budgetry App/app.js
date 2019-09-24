ddc25a0e2c8c717ee9bf53167c794fb859552fcb
Jahanzeb Malik <jahanxeb.m@gmail.com>
2019-09-24 22:53:35 +0500

    modules and controller

--- Budgetry App/app.js
+++ Budgetry App/app.js
@@ -1,11 +1,18 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
-    var x = 23;
-    var add = function (a) {
-        return x + a;
-    }
-    return {
-        publicTest: function (b) {
-            console.log(add(b));
-        }
-    }
+
+    //Some Code
 })();
+
+
+var UIController = (function () {
+
+    //Some Code
+
+})();
+
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+
+})(budgetController, UIController);
